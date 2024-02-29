b430330b087a2bf669d31aed9c46b3ea721bf94d
Guido Rann <guidorann.code@gmail.com>
2024-02-28 21:44:46 -0300

    agregando controller

---
+++ src/main/java/com/sage/ClinicaSage/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.sage.ClinicaSage.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(value = "http://localhost:5173")
+@RestController
+@RequestMapping("clinica-sage")
+public class UserController {
+}
